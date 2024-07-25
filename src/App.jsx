eed0c8039f251f2e82bdd1674790a4015a26821f
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-07-25 11:49:48 +0545

    sign in component and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './components/SignIn';
+
 function App() {
-  return <div>App</div>
+  return (
+    <Router>
+      <Routes>
+        <Route path='/login' element={<SignIn/>}></Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
